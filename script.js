9642089ca82e239aeb06e7c8f41f2a2c0a69f8d3
Kofi Kusi Appau <appaukusi05@gmail.com>
2025-08-06 21:49:41 +0000

    Add files

---
+++ script.js
@@ -0,0 +1,20 @@
+async function getAdvice() {
+    const id = document.querySelector("span")
+    const advice = document.querySelector("p")
+
+    const response = await fetch("https://api.adviceslip.com/advice")
+    const data = response.json()
+    data.then(function(res){
+        id.textContent = res.slip.id
+        advice.textContent = res.slip.advice
+    })
+    .catch(function(err){
+        alert(err)
+    })
+
+
+}
+
+
+
+
